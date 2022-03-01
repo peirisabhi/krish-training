0696ff000e74293f37e7f32399ab3a9176494402
abhishek dananjaya <abhishekdananjaya1@yahoo.com>
2022-03-01 12:20:14 +0530

    re upload wickeys problem

---
+++ wickeys_problem/Application.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Application{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Distance");
+
+		int distance = scanner.nextInt();
+
+		System.out.println(distance);
+		int x = 0;
+		int time = 0;
+		while(true){
+			x += 5;
+			time += 1;
+
+			if(x >= distance)
+			  break;	
+
+			x += 3;
+			time += 2;
+
+			if(x >= distance)
+			  break;
+
+			x += 1;
+			time += 3;
+
+			if(x >= distance)
+			  break;
+
+		}
+		System.out.println(x);
+		System.out.println("Time is " + time);
+
+	}
+
+}
