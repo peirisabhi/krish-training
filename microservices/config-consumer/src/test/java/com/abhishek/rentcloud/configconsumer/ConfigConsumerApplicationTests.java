6f58915acc6eedb326937cd59c05fbf4062b6bde
abhishek dananjaya <abhishekdananjaya1@yahoo.com>
2022-04-23 12:20:51 +0530

    config consumer added & working

---
+++ microservices/config-consumer/src/test/java/com/abhishek/rentcloud/configconsumer/ConfigConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.rentcloud.configconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
